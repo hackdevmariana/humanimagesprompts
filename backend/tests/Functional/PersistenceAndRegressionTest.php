edf8c5ced9fa725cc26d68c03b27c4614fe0ef6f
Fernando <fernando@juandemariana.org>
2026-08-17 01:27:05 +0200

    test: fix test DB config and update expected tables count

--- backend/tests/Functional/PersistenceAndRegressionTest.php
+++ backend/tests/Functional/PersistenceAndRegressionTest.php
@@ -23,11 +23,12 @@ class PersistenceAndRegressionTest extends DatabaseTestCase
         'pose',
         'prompt_composition',
         'scene',
+        'time_weather',
     ];
 
     private const UUID_V4 = '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
 
-    public function testEightTablesAreCreatedFromMappings(): void
+    public function testNineTablesAreCreatedFromMappings(): void
     {
         $tables = array_map(
             'strtolower',
