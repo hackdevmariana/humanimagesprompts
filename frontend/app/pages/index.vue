f94f8f905404d7d8f264130399770b4b2e24d8eb
Fernando <fernando@juandemariana.org>
2026-08-16 23:30:10 +0200

    feat(frontend): add index page

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+const auth = useAuthStore();
+
+if (!auth.initialized) {
+  await auth.fetchSession();
+}
+
+if (auth.isAuthenticated) {
+  navigateTo('/dashboard');
+} else {
+  navigateTo('/login');
+}
+</script>
