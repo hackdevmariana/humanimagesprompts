1db9daa8bfe145af1ee77734582bdc97329fa164
Fernando <fernando@juandemariana.org>
2026-08-17 20:40:12 +0200

    feat(frontend): add character dedicated page

---
+++ frontend/app/pages/character.vue
@@ -0,0 +1,20 @@
+<template>
+  <AreaLayout title="Personaje" icon="PhUserCircle" blockKey="character">
+    <template #sidebar>
+      <AreaSidebar :block-key="blockKey" :store="store" />
+    </template>
+    <template #editor>
+      <CharacterEditor />
+    </template>
+  </AreaLayout>
+</template>
+
+<script setup lang="ts">
+import AreaLayout from '@/layouts/AreaLayout.vue';
+import AreaSidebar from '@/components/editor/AreaSidebar.vue';
+import CharacterEditor from '@/components/editor/CharacterEditor.vue';
+import { useCharacterStore } from '@/stores/character';
+
+const blockKey = 'character';
+const store = useCharacterStore();
+</script>
